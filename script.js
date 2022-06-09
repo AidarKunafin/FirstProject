7ade91dcf3b08d9571ca24ff0fbe4e1d540a6293
AidarKunafin <89005327+AidarKunafin@users.noreply.github.com>
2022-06-09 23:06:10 +0500

    object lesson

--- script.js
+++ script.js
@@ -1,57 +1,15 @@
 'use strict'
-let indexGlobal = 1
-let questons = [
-    {
-        id: 1,
-        title: 'Как преобразовать строку в верхний регистр?',
-        answer: ['uppercase', 'lowercase', 'push', 'pop']
-    },
-    {
-        id: 2,
-        title: 'Какой метод window показывает сообщение и запрашивает ввод текст пользователя?',
-        answer: ['promt', 'alert', 'confirm', 'map']
-    },
-    {
-        id: 3,
-        title: 'Для чего используется оператор ||?',
-        answer: ['сравнение', 'деление', 'и', 'или']
-    }
-]
 
-function template(quetion) {
-    return `
-        <div id="block">
-            <h1>
-                ${quetion.title}
-            </h1>
-            <form>
-                ${quetion.answer.map(element => {
-                    return `
-                        <input type="radio" onchange="alert(this.value)">
-                            ${element}
-                        </input>
-                    `
-                }).join('')}
-            </form>
-        </div>
-    `
-}
-function add(id = 1) {
-   root.insertAdjacentHTML('afterbegin', template(questons[id - 1]))
-}
-
-add(indexGlobal)
-
-function deleteQuestion(){
-    block.remove()
-    console.log(indexGlobal)
-}
+let obj = {
+    name: 'Aidar', 
+    age: 29
+};
 
 
-button.onclick = function(e){
-    e.preventDefault()
-    indexGlobal += 1;
-    deleteQuestion()
-    add(indexGlobal)
-}
+// let obj2 = Object.assign(obj)
+// casw
+// values
+// entris
 
+
+console.log(obj)
