20e5e0ec5d5ea4785d3764363786661c068e5b76
kunafin92 <aidar-iceman@mail.ru>
2022-06-14 17:47:20 +0500

    version 1.0.1r

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
 'use strict'
 let right = 0;
 let indexGlobal = 1
+const theme = [
+
+]
 let questons = [
     {
         id: 1,
         title: 'Как преобразовать строку в верхний регистр?',
-        answer: ['toUppercase', 'toLowercase', 'push', 'pop']
+        answer: ['toUppercase', 'toLowercase', 'push', 'pop'],
+        definition: 'метод toUpperCase() возвращает строку в верхнем регистре',
+        correctAnswer: 'toUppercase'
     },
     {
         id: 2,
         title: 'Какой метод window показывает сообщение и запрашивает ввод текст пользователя?',
-        answer: ['prompt', 'alert', 'confirm', 'map']
+        answer: ['prompt', 'alert', 'confirm', 'map'],
+        definition: 'promt',
+        correctAnswer: 'prompt'
     },
     {
         id: 3,
         title: 'Для чего используется оператор ||?',
-        answer: ['сравнение', 'деление', 'и', 'или']
-    }
-]
-
-let correctAnswer = [
-    {
-        answer: 'toUppercase',
-        definition: 'метод toUpperCase() возвращает строку в верхнем регистре'
-    },
-    {
-        answer: 'prompt',
-        definition: 'promt'
-    },
-    {
-        answer: 'или',
-        definition: '|| оператор «ИЛИ»'
+        answer: ['сравнение', 'деление', 'и', 'или'],
+        definition: '|| оператор «ИЛИ»',
+        correctAnswer: 'или'
     }
 ]
 
@@ -73,7 +67,7 @@ function completedTest(){
 
 function addAnswer(){
     main.insertAdjacentHTML('afterend', `<div class="answer-block" id="answerBlock">
-                            <div class="definition" id="definition">Верный ответ:${correctAnswer[indexGlobal-1].definition}</div>
+                            <div class="definition" id="definition">Верный ответ:${questons[indexGlobal-1].definition}</div>
                             <div class="nextButtonBlock"><button class="nextButton" id="nextButton">Следующий вопрос</button></div>
                             </div>`);
 }
@@ -105,7 +99,7 @@ function e(){
     let trueAnswer = document.querySelectorAll("input[type='radio']")
     for (let radio of trueAnswer) {
 		if (radio.checked) {
-            if(correctAnswer[indexGlobal-1].answer == radio.value){
+            if(questons[indexGlobal-1].correctAnswer == radio.value){
                 // block.style.backgroundColor = "green";
                 right += 1;
                 if (indexGlobal < questons.length){
@@ -127,7 +121,4 @@ function e(){
 }
 
 button.addEventListener('click', e)
-
-
-
 
